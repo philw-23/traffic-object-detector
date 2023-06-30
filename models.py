8afec1610491192a711933fd5c596ea192a242d1
phil <philw16@uw.edu>
2023-06-30 14:00:57 -0700

    Updated to log to local text file to avoid issues with logger when resuming training

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from typing import Any
+import os
 import torch
 import pytorch_lightning as pl
+from torchvision.ops import MultiScaleRoIAlign
 from torchvision.models import get_model
-from torchvision.models.detection import RetinaNet, FasterRCNN
+from torchvision.models.detection import RetinaNet, FasterRCNN, backbone_utils
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 
@@ -13,43 +15,60 @@ base_models = {
 }
 
 class LightningModel(pl.LightningModule):
-    def __init__(self, model, optimizer):
+    def __init__(self, model, optimizer, log_file):
         super().__init__()
         self.model = model
         self.optimizer = optimizer
+        self.log_file = log_file
         self.map_metric = MeanAveragePrecision()
-
-    def forward(self, x):
-        return self.model(x)
     
     def training_step(self, batch, batch_idx):
-        images, boxes, labels = batch
+        # Execute train step
+        images, targets = batch
         images = [image for image in images]
-        targets = [{'boxes':boxes[i], 'labels':labels[i]} for i in range(len(labels))]
+        targets = [{k:v for k, v in t.items()} for t in targets] # list of dicts
         loss_dict = self.model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
-        self.log('train_loss', losses, on_step=False, on_epoch=True) # Log loss on epoch end
+        self.log('train_loss', losses, on_step=False, on_epoch=True) 
+
+        # Return
         return losses
     
     def validation_step(self, batch, batch_idx):
         # Set to train to calcualte loss
         self.model.train()
-        images, boxes, labels = batch
+        images, targets = batch
         images = [image for image in images]
-        targets = [{'boxes':boxes[i], 'labels':labels[i]} for i in range(len(labels))]
+        targets = [{k:v for k, v in t.items()} for t in targets] # list of dicts
         loss_dict = self.model(images, targets)
         losses = sum(loss for loss in loss_dict.values())
-        self.log('val_loss', losses)
+        self.log('val_loss', losses, on_step=False, on_epoch=True)
 
         # Evaluate
         self.model.eval()
         val_outputs = self.model(images)
-        self.map_metric(val_outputs, targets)
+        self.map_metric.update(val_outputs, targets)
 
     def on_validation_epoch_end(self):
+        # Compute metrics
         metrics = self.map_metric.compute()
-        for k, v in metrics.items():
-            self.log(k, v.item())
+
+        # Write to log file
+        if os.path.isfile(self.log_file): # Exists, we want to append
+            write_mode = 'a' # Append
+        else:
+            write_mode = 'w' # Create
+            
+        with open(self.log_file, write_mode) as out:
+            out.write(f'epoch={self.current_epoch}\t')
+            for k, v in self.trainer.logged_metrics.items(): # Losses
+                out.write(f'{k}={v}\t')
+            for k, v in metrics.items(): # mAP metrics
+                out.write(f'{k}={v}\t')
+            out.write('\n')
+
+        # Reset metric/epoch log
+        self.map_metric.reset()
 
     def configure_optimizers(self) -> Any:
         return self.optimizer
@@ -66,22 +85,16 @@ def get_model_items(model_id, num_classes, optimizer, trainable_backbone_layers=
 
         else:
             try: # Try to develop model with the backbone        
-                backbone_model = get_backbone(backbone_id, backbone_out_channels)
-                # NOTE: Need to define anchor generator for general model class calls to work
-                anchor_generator = AnchorGenerator( 
-                    sizes=((32, 64, 128, 256, 512),),
-                    aspect_ratios=((0.5, 1.0, 2.0),)
-                )
-
+                backbone_model = get_backbone(backbone_id, backbone_out_channels) # Get backbone
+                
+                # Note: Using default anchor generator for each model class
                 if model_id == 'RetinaNet':
                     model = RetinaNet(backbone=backbone_model, 
-                                        num_classes=num_classes,
-                                        anchor_generator=anchor_generator)
+                                        num_classes=num_classes)
 
-                else:
+                else: # For FasterRCNN - also need to define roi_pooler
                     model = FasterRCNN(backbone=backbone_model, 
-                                        num_classes=num_classes,
-                                        rpn_anchor_generator=anchor_generator)
+                                       num_classes=num_classes)
 
             except:
                 print('Unable to substitute backbone - preceeding with default')
@@ -113,11 +126,8 @@ def get_model_items(model_id, num_classes, optimizer, trainable_backbone_layers=
 def get_backbone(backbone_id, backbone_out_channels):
 
     if 'resnet' in backbone_id:
-        backbone_model = get_model(backbone_id, weights='DEFAULT')
-        backbone_layers = list(backbone_model.children())[:-1] # NOTE: iterating over named modules didn't work
-        backbone_model = torch.nn.Sequential(*backbone_layers)
-        backbone_model.out_channels = backbone_out_channels
-        
+        backbone_model = backbone_utils.resnet_fpn_backbone(backbone_id, 
+                                                            weights='DEFAULT') # Pretrained Backbone
         return backbone_model
 
     return # No backbone substitution available
